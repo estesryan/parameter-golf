c19fc0adf13eb2db9358e2b3ec5c58daa026cc32
Ryan Estes <estes.ryan@gmail.com>
2026-04-07 21:22:05 +0100

    CONTROL_LR 0.8

--- train_gpt_skip.py
+++ train_gpt_skip.py
@@ -52,7 +52,7 @@ class Hyperparameters:
 
     # Training length.
     iterations = int(os.environ.get("ITERATIONS", 20000))
-    warmdown_fraction = float(os.environ.get("WARMDOWN_FRACTION", 1.0))
+    warmdown_fraction = float(os.environ.get("WARMDOWN_FRACTION", 0.9))
     warmup_steps = int(os.environ.get("WARMUP_STEPS", 20))
     train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 524_288))
     train_seq_len = int(os.environ.get("TRAIN_SEQ_LEN", 1024))
