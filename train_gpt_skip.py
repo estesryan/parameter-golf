ba2ff7412b720250d2e632eb14772ef09710146e
Ryan Estes <estes.ryan@gmail.com>
2026-04-07 10:43:30 +0100

    no attn_scale, no mlp_scale

--- train_gpt_skip.py
+++ train_gpt_skip.py
@@ -632,8 +632,8 @@ class Block(nn.Module):
         self.mlp_norm = RMSNorm()
         self.attn = CausalSelfAttention(dim, num_heads, num_kv_heads, rope_base, qk_gain_init)
         self.mlp = MLP(dim, mlp_mult)
-        self.attn_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
-        self.mlp_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
+        self.register_buffer("attn_scale", torch.ones(dim, dtype=torch.float32), persistent=False)
+        self.register_buffer("mlp_scale", torch.ones(dim, dtype=torch.float32), persistent=False)
         self.resid_mix = nn.Parameter(torch.stack((torch.ones(dim), torch.zeros(dim))).float())
 
     def forward(self, x: Tensor, x0: Tensor) -> Tensor:
