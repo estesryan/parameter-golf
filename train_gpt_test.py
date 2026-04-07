401615a0c8896bc8423133f5e8c9f8f71c533dc0
Ryan Estes <estes.ryan@gmail.com>
2026-04-07 08:01:34 +0100

    base

--- train_gpt_test.py
+++ train_gpt_test.py
@@ -628,7 +628,7 @@ class Block(nn.Module):
         self.attn = CausalSelfAttention(dim, num_heads, num_kv_heads, rope_base, qk_gain_init)
         self.mlp = MLP(dim, mlp_mult)
         self.attn_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
-        self.register_buffer("mlp_scale", torch.ones(dim, dtype=torch.float32), persistent=False)
+        self.mlp_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
         self.resid_mix = nn.Parameter(torch.stack((torch.ones(dim), torch.zeros(dim))).float())
 
     def forward(self, x: Tensor, x0: Tensor) -> Tensor:
