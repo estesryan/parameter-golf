0dc36129e5f8517b457d6f63bfad7af8887da880
Ryan Estes <estes.ryan@gmail.com>
2026-04-08 19:11:25 +0100

    update token mixer

--- train_gpt_exp.py
+++ train_gpt_exp.py
@@ -619,7 +619,12 @@ class MLP(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         x = torch.relu(self.fc(x))
         x = x.square()
-        x = x + 0.08 * torch.roll(x, shifts=1, dims=1) + 0.04 * torch.roll(x, shifts=2, dims=1)
+        x = (
+            x
+            + 0.08 * torch.roll(x, shifts=1, dims=1)
+            + 0.04 * torch.roll(x, shifts=2, dims=1)
+            + 0.02 * torch.roll(x, shifts=3, dims=1)
+        )
         return self.proj(x)
 
 
