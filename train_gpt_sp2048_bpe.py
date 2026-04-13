d87358e44ccda55e995f0a3c8628e2771f39a9df
Ryan Estes <estes.ryan@gmail.com>
2026-04-13 20:59:25 +0100

    parallel block

--- train_gpt_sp2048_bpe.py
+++ train_gpt_sp2048_bpe.py
@@ -638,9 +638,14 @@ class Block(nn.Module):
         mix = self.resid_mix.to(dtype=x.dtype)
         x = mix[0][None, None, :] * x + mix[1][None, None, :] * x0
 
+        # parallel paths
         attn_out = self.attn(self.attn_norm(x))
-        x = x + self.attn_scale.to(dtype=x.dtype)[None, None, :] * attn_out
-        x = x + self.mlp_scale.to(dtype=x.dtype)[None, None, :] * self.mlp(self.mlp_norm(x))
+        mlp_out = self.mlp(self.mlp_norm(x))
+
+        x = x + (
+            self.attn_scale.to(dtype=x.dtype)[None, None, :] * attn_out
+            + self.mlp_scale.to(dtype=x.dtype)[None, None, :] * mlp_out
+        )
         return x
 
 
