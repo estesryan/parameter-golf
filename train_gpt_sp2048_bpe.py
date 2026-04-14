80251ea926e530d76a55c950f77f24a4a826ee63
Ryan Estes <estes.ryan@gmail.com>
2026-04-14 16:48:52 +0100

    global mlp scale

--- train_gpt_sp2048_bpe.py
+++ train_gpt_sp2048_bpe.py
@@ -633,19 +633,16 @@ class Block(nn.Module):
         self.attn_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
         self.mlp_scale = nn.Parameter(torch.ones(dim, dtype=torch.float32))
         self.resid_mix = nn.Parameter(torch.stack((torch.ones(dim), torch.zeros(dim))).float())
+        self.mlp_global_scale = nn.Parameter(torch.tensor(1.0, dtype=torch.float32))
 
     def forward(self, x: Tensor, x0: Tensor) -> Tensor:
         mix = self.resid_mix.to(dtype=x.dtype)
         x = mix[0][None, None, :] * x + mix[1][None, None, :] * x0
 
-        x_in = x
-        attn_out = self.attn(self.attn_norm(x_in))
-        mlp_out = self.mlp(self.mlp_norm(x_in))
+        attn_out = self.attn(self.attn_norm(x))
+        x = x + self.attn_scale.to(dtype=x.dtype)[None, None, :] * attn_out
 
-        x = x_in + 0.85 * (
-            self.attn_scale.to(dtype=x.dtype)[None, None, :] * attn_out
-            + self.mlp_scale.to(dtype=x.dtype)[None, None, :] * mlp_out
-        )
+        x = x + self.mlp_global_scale.to(dtype=x.dtype) * self.mlp_scale.to(dtype=x.dtype)[None, None, :] * self.mlp(self.mlp_norm(x))
         return x
 
 
