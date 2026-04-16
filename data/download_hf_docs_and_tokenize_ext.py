77a6305a82008b11392d22531628439aa780bff0
Ryan Estes <estes.ryan@gmail.com>
2026-04-16 10:32:38 +0100

    sp4096

--- data/download_hf_docs_and_tokenize_ext.py
+++ data/download_hf_docs_and_tokenize_ext.py
@@ -8,6 +8,12 @@ Command:
 
 PYTHONUNBUFFERED=1 python3 data/download_hf_docs_and_tokenize_ext.py \
   --output-root ./data \
+  --tokenizer-config ./data/tokenizer_specs_4096.json \
+  --tokenizer-train-docs 1000000 \
+  --train-shards 80
+
+PYTHONUNBUFFERED=1 python3 data/download_hf_docs_and_tokenize_ext.py \
+  --output-root ./data \
   --tokenizer-config ./data/tokenizer_specs_3072.json \
   --tokenizer-train-docs 1000000 \
   --train-shards 80
