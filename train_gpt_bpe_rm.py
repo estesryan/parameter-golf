1424cb3724f2b687b52f868a9bcf1e6a4eb397db
Ryan Estes <estes.ryan@gmail.com>
2026-04-18 07:15:00 +0100

    bugfix

--- train_gpt_bpe_rm.py
+++ train_gpt_bpe_rm.py
@@ -970,8 +970,8 @@ def main() -> None:
             module.float()
     restore_low_dim_params_to_fp32(base_model)
     #compiled_model = torch.compile(base_model, dynamic=False, fullgraph=True)
-    compiled_model = torch.compile(base_model, dynamic=False)
-    model: nn.Module = DDP(compiled_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else compiled_model
+    #model: nn.Module = DDP(compiled_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else compiled_model
+    model: nn.Module = DDP(base_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else base_model
 
     # Optimizer split:
     # - token embedding (Adam) uses EMBED_LR (untied) or TIED_EMBED_LR (tied)
