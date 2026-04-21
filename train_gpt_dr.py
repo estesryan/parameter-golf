1253b88b7363b182778a7ac66f95effef59c4088
Ryan Estes <estes.ryan@gmail.com>
2026-04-21 19:44:52 +0100

    settings

--- train_gpt_dr.py
+++ train_gpt_dr.py
@@ -69,7 +69,7 @@ class Hyperparameters:
 
     # Model shape.
     vocab_size = int(os.environ.get("VOCAB_SIZE", 5120))
-    num_layers = int(os.environ.get("NUM_LAYERS", 4))
+    num_layers = int(os.environ.get("NUM_LAYERS", 5))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 4))
     model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_heads = int(os.environ.get("NUM_HEADS", 8))
