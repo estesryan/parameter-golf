947c2ccdcfa1762e294b651af6724489e70171ca
Ryan Estes <estes.ryan@gmail.com>
2026-04-23 16:31:11 +0100

    dynamic

--- train_gpt_ssm_hybrid2.py
+++ train_gpt_ssm_hybrid2.py
@@ -949,7 +949,7 @@ def main() -> None:
         if isinstance(module, CastedLinear):
             module.float()
     restore_low_dim_params_to_fp32(base_model)
-    compiled_model = torch.compile(base_model, dynamic=False)
+    compiled_model = torch.compile(base_model, dynamic=True)
     model: nn.Module = DDP(compiled_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else compiled_model
 
     # Optimizer split:
