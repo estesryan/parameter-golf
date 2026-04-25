fa9d6f689f17c97f4aa75ac3d898be3304a38b8b
AzureAD\RyanEstes <restes@ccint.net>
2026-04-25 16:43:31 +0100

    128

--- train_gpt_ssm_hybrid.py
+++ train_gpt_ssm_hybrid.py
@@ -66,7 +66,7 @@ class Hyperparameters:
     ssm_mlp_mult = float(os.environ.get("SSM_MLP_MULT", 2))
     ssm_layers = os.environ.get("SSM_LAYERS", "7")
     no_attn_layers = os.environ.get("NO_ATTN_LAYERS", "3,6")
-    ssm_state_dim = int(os.environ.get("SSM_STATE_DIM", 64))
+    ssm_state_dim = int(os.environ.get("SSM_STATE_DIM", 128))
     ssm_num_groups = int(os.environ.get("SSM_NUM_GROUPS", 8))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "0")))
     rope_base = float(os.environ.get("ROPE_BASE", 10000.0))
