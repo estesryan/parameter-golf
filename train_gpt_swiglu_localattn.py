43b8723dcc592c93f415869086ef969a63d61ff5
Ryan Estes <estes.ryan@gmail.com>
2026-04-28 15:06:16 +0100

    hidden

--- train_gpt_swiglu_localattn.py
+++ train_gpt_swiglu_localattn.py
@@ -615,14 +615,15 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, dim: int, mlp_mult: int):
         super().__init__()
-        hidden = mlp_mult * dim
-        self.fc = CastedLinear(dim, hidden, bias=False)
+        hidden = int((mlp_mult * dim) * 2 / 3)
+        hidden = ((hidden + 63) // 64) * 64
+        self.gate = CastedLinear(dim, hidden, bias=False)
+        self.up = CastedLinear(dim, hidden, bias=False)
         self.proj = CastedLinear(hidden, dim, bias=False)
         self.proj._zero_init = True
 
     def forward(self, x: Tensor) -> Tensor:
-        x = torch.relu(self.fc(x))
-        return self.proj(x.square())
+        return self.proj(F.silu(self.gate(x)) * self.up(x))
 
 
 class Block(nn.Module):
