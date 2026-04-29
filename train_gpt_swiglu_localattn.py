820d578090f120bff2eeffd33dacc97e4a2fd379
Ryan Estes <estes.ryan@gmail.com>
2026-04-29 13:32:13 +0100

    revert

--- train_gpt_swiglu_localattn.py
+++ train_gpt_swiglu_localattn.py
@@ -553,8 +553,7 @@ class DistributedTokenLoader:
         per_rank_span = local_tokens + 1
         #chunk = self.stream.take(per_rank_span * self.world_size)
         # sequence packing with random offsets
-        #offset = random.randint(0, seq_len - 1)
-        offset = random.randint(0, 511)
+        offset = random.randint(0, seq_len - 1)
         chunk = self.stream.take(offset + per_rank_span * self.world_size)
         chunk = chunk[offset:]
         start = self.rank * per_rank_span
